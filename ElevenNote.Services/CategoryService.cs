820904b49f02bfae4e037ff58561a11f1885446b
drewawright <drewawright@gmail.com>
2019-06-26 14:55:52 -0400

    Category CRUD finished

--- ElevenNote.Services/CategoryService.cs
+++ ElevenNote.Services/CategoryService.cs
@@ -73,5 +73,19 @@ namespace ElevenNote.Services
                 return ctx.SaveChanges() == 1;
             }
         }
+
+        public bool DeleteCategory(int categoryId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categories
+                        .Single(e => e.CategoryId == categoryId);
+                ctx.Categories.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
--- ElevenNote.WebMVC/Controllers/CategoryController.cs
+++ ElevenNote.WebMVC/Controllers/CategoryController.cs
@@ -40,5 +40,66 @@ namespace ElevenNote.WebMVC.Controllers
             }
             return View(model);
         }
+
+        //GET: Category/Edit/{id}
+        public ActionResult Edit(int id)
+        {
+            var service = new CategoryService();
+            var update = service.GetCategoryById(id);
+
+            var model = new CategoryEdit
+            {
+                CategoryId = update.CategoryId,
+                CategoryName = update.CategoryName
+            };
+            return View(model);
+        }
+
+        //POST: Category/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, CategoryEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (model.CategoryId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+
+            var service = new CategoryService();
+            if (service.UpdateCategory(model))
+            {
+                TempData["SaveResult"] = "Category Updated";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Category could not be updated");
+            return View(model);
+        }
+
+        //GET: Category/Delete/{id}
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var service = new CategoryService();
+            var model = service.GetCategoryById(id);
+
+            return View(model);
+        }
+
+        //POST: Category/Delete/{id}
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteCategory(int id)
+        {
+            var service = new CategoryService();
+            service.DeleteCategory(id);
+            TempData["SaveResult"] = "Category Deleted";
+
+            return RedirectToAction("Index");
+        }
     }
 }
