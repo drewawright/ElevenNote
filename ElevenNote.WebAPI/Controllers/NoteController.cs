e3b5132b30994d94299807c27c59b2cc517ac914
drewawright <drewawright@gmail.com>
2019-07-23 15:34:39 -0400

    starting to add api, branch created

---
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,24 @@
+﻿using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    public class NoteController : ApiController
+    {
+
+        private NoteService CreateNoteService()
+        {
+            var userID = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new NoteService(userID);
+
+            return noteService;
+        }
+    }
+}
